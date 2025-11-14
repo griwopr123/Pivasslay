003485fceecbeb3fc865f70d66f52b34f25853b8
griwopr123 <griwopr123@gmail.com>
2025-11-14 23:57:08 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import { Header } from './components/header/Header.tsx'
+import './fonts.scss'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
